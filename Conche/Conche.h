cc7892fd63ccb73baed89ebe7da88d396d585229
Dan Stenmark <xelinis@Dans-iMac.local>
2015-05-30 01:43:55 -0700

    Initial commit

---
+++ Conche/Conche.h
@@ -0,0 +1,19 @@
+//
+//  Conche.h
+//  Conche
+//
+//  Created by Dan Stenmark on 5/30/15.
+//  Copyright (c) 2015 Dan Stenmark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Conche.
+FOUNDATION_EXPORT double ConcheVersionNumber;
+
+//! Project version string for Conche.
+FOUNDATION_EXPORT const unsigned char ConcheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Conche/PublicHeader.h>
+
+
